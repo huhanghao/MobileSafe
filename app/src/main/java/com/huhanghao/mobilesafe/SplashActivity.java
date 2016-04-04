b68af0679bf0c55d240f6a452c97d6c104d8d162
huhanghao <517072971@qq.com>
2016-04-04 13:01:30 +0800

    Initial commit

---
+++ app/src/main/java/com/huhanghao/mobilesafe/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.huhanghao.mobilesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
